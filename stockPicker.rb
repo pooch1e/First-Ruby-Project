db145b7c6b682ecb727473e520c02b7e7a61e4cc
Joel Kram <joelnskram@gmail.com>
2024-11-26 17:02:27 +0000

    added stock picker problem

---
+++ stockPicker.rb
@@ -0,0 +1,10 @@
+#Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day. It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
+#
+#psudo it - look through whole list and give index
+# note index that has lowest and highest
+# rule is lowest has to come before highest
+# 
+stock = [17,3,6,9,15,8,6,1,10]
+def stock_picker(stock)
+  
+end
