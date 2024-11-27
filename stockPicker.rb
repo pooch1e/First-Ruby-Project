1e495e2ce0a44b7c1c67b4d2c347f94c3cc9cec2
Joel Kram <joelnskram@gmail.com>
2024-11-27 20:04:46 +0000

    finished up and tested problem

--- stockPicker.rb
+++ stockPicker.rb
@@ -1,19 +1,44 @@
 #Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day. It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
 #
 # 
-# variable should hold x of array which is current day. it should then compare next day to x. If nexdt day is higher than x then dont buy, if it is lower then exchange with x - iterate through whole array doing this 
-# once that is finished, go back and find the highest number after X which will be the sell day. following the same method but reversed to find highest int
+# 1.variable should hold x of array which is current day. it should then compare next day to x.
+# 2. If nexdt day is higher than x then dont buy, if it is lower then exchange with x - iterate through whole array doing this 
+#3. once that is finished, go back and find the highest number after X which will be the sell day. following the same method but reversed to find highest int
 #
 # 
-stock = [17,3,6,9,15,8,6,1,10]
-def stock_picker(stck)
-  # index of best stock to go in these arrays?
-  day_to_buy = stck.to_a
-  day_to_sell = stck.to_a
+stock = [17, 3, 6, 9, 15, 8, 6, 1, 10]
 
-  worst_day = day_to_sell.min
-  best_day = day_to_buy.max
-  p "the best day was #{best_day} and the worst day was #{worst_day}"
+def stock_picker(stock)
+  # Initialize variables
+  current_day_price = stock[0]
+  current_day_index = 0
 
+  max_profit = 0
+  sell_day = 0
+  buy_day = 0
+
+  # Iterate through the stock prices
+  stock.each_with_index do |price, index|
+    # Update the minimum price and the corresponding buy day
+    if price < current_day_price
+      current_day_price = price
+      current_day_index = index
+    end
+
+    # Calculate the potential profit for selling on the current day
+    potential_profit = price - current_day_price
+
+    # Update max_profit, sell_day, and buy_day if a better profit is found
+    if potential_profit > max_profit
+      max_profit = potential_profit
+      sell_day = index
+      buy_day = current_day_index
+    end
+  end
+
+  puts "The best price to buy is: #{stock[buy_day]} on day #{buy_day}"
+  puts "The best day to sell is day #{sell_day} at price #{stock[sell_day]}"
+  puts "The maximum profit is: #{max_profit}"
 end
+
 stock_picker(stock)
