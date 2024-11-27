789443d27eda0fc929c16dfbbf9e09253cb006e8
Joel Kram <joelnskram@gmail.com>
2024-11-27 19:00:16 +0000

    figured out what im doing i think with psudo code

--- firstfile.rb
+++ firstfile.rb
@@ -24,14 +24,41 @@
 # your_name = gets
 # 
 #methods
-def greeting(name)
-  name.to_s
-  "Hello #{name}"
+# def greeting(name)
+#   name.to_s
+#   "Hello #{name}"
+# end
+
+# puts greeting("joel")
+# def multiply(a, b)
+#   return a*b
+# end
+
+# puts multiply(10, 11)
+
+
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+# Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in.
+
+# Additionally, if the number is negative, return 0.
+
+# Note: If the number is a multiple of both 3 and 5, only count it once.
+# 
+
+def add_it_up(number)
+  output_array = []
+  number_array = (1..number-1).to_a
+  number_array.each do |n|
+    if n % 3 == 0 || n % 5 == 0
+      output_array.push(n)
+    end
+  end
+  result = output_array.sum
+  if result <= 0
+    p 0
+    return
+  end
 end
 
-puts greeting("joel")
-def multiply(a, b)
-  return a*b
-end
-
-puts multiply(10, 11)
+add_it_up(10)
--- stockPicker.rb
+++ stockPicker.rb
@@ -1,10 +1,19 @@
 #Implement a method #stock_picker that takes in an array of stock prices, one for each hypothetical day. It should return a pair of days representing the best day to buy and the best day to sell. Days start at 0.
 #
-#psudo it - look through whole list and give index
-# note index that has lowest and highest
-# rule is lowest has to come before highest
+# 
+# variable should hold x of array which is current day. it should then compare next day to x. If nexdt day is higher than x then dont buy, if it is lower then exchange with x - iterate through whole array doing this 
+# once that is finished, go back and find the highest number after X which will be the sell day. following the same method but reversed to find highest int
+#
 # 
 stock = [17,3,6,9,15,8,6,1,10]
-def stock_picker(stock)
-  
+def stock_picker(stck)
+  # index of best stock to go in these arrays?
+  day_to_buy = stck.to_a
+  day_to_sell = stck.to_a
+
+  worst_day = day_to_sell.min
+  best_day = day_to_buy.max
+  p "the best day was #{best_day} and the worst day was #{worst_day}"
+
 end
+stock_picker(stock)
